b3878b3633a7433078b78eabe3a2a9c40999476a
Ala <alatech.dev@gmail.com>
2025-03-14 04:01:32 +0300

    "feat(auth): check onboarding token to redirect between auth and onboarding flows"

--- lib/core/ath/auth_checker.dart
+++ lib/core/ath/auth_checker.dart
@@ -1,36 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:trendfashion/core/util/app_router.dart';
 import 'package:trendfashion/core/ath/token_storage.dart';
+import 'package:trendfashion/features/Onboarding/presintation/data/token_storage_Onboarding.dart';
+import 'package:trendfashion/core/util/app_router.dart';
 
 class AuthChecker extends StatelessWidget {
   const AuthChecker({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<String?>(
-      future: TokenStorage.getToken(),
+    return FutureBuilder(
+      future: Future.wait([
+        TokenStorage.getToken(),
+        OnboardingStorage.isCompleted(),
+      ]),
       builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Scaffold(
-            // Could Be a custom Loading Baby
-            body: Center(child: CircularProgressIndicator()),
-          );
+        if (snapshot.connectionState != ConnectionState.done) {
+          return _buildLoadingScreen();
         }
 
-        if (snapshot.hasData) {
-          WidgetsBinding.instance.addPostFrameCallback((_) {
-            Navigator.pushReplacementNamed(context, AppRouter.categories);
-          });
-        } else {
-          WidgetsBinding.instance.addPostFrameCallback((_) {
-            Navigator.pushReplacementNamed(context, AppRouter.categories);
-          });
-        }
+        final token = snapshot.data?[0] as String?;
+        final onboardingCompleted = snapshot.data?[1] as bool? ?? false;
 
-        return const Scaffold(
-          body: Center(child: CircularProgressIndicator()),
-        );
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          _handleNavigation(context, token, onboardingCompleted);
+        });
+
+        return _buildLoadingScreen();
       },
     );
   }
+
+  void _handleNavigation(
+      BuildContext context, String? token, bool onboardingCompleted) {
+    if (token != null) {
+      print('AuthChecker - Has Token: $token');
+      print('AuthChecker - Onboarding Completed: $onboardingCompleted');
+      Navigator.pushReplacementNamed(context, AppRouter.categories);
+    } else {
+      print(onboardingCompleted
+          ? 'Navigating to SignUp'
+          : 'Navigating to Onboarding');
+      final route =
+          onboardingCompleted ? AppRouter.signUp : AppRouter.onboarding;
+      Navigator.pushReplacementNamed(context, route);
+    }
+  }
+
+  Widget _buildLoadingScreen() => const Scaffold(
+        body: Center(child: CircularProgressIndicator()),
+      );
 }
