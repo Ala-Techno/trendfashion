72c8fb33df30c18cc593cf724e0b02d1c60349ad
Ala <alatech.dev@gmail.com>
2025-03-14 05:35:00 +0300

    "refactor(router): migrate to dynamic route generation and update route constants"

--- lib/core/util/app_router.dart
+++ lib/core/util/app_router.dart
@@ -1,28 +1,36 @@
+import 'package:flutter/material.dart';
 import 'package:trendfashion/core/ath/auth_checker.dart';
-import 'package:trendfashion/features/Home/presintation/page/HomePage.dart';
-import 'package:trendfashion/features/Introdaction/pages/firstPage.dart';
 import 'package:trendfashion/features/Onboarding/presintation/page/OnboardingPage.dart';
 
 import 'package:trendfashion/features/Product/presintation/page/productsPage.dart';
 import 'package:trendfashion/features/Registration/presintation/page/LoginPage.dart';
 import 'package:trendfashion/features/Registration/presintation/page/SingUp.dart';
+import 'package:trendfashion/features/categories/presintation/page/categoriesPage.dart';
 
 class AppRouter {
-  static const String initial = '/';
-  static const String firstPage = '/first';
+  static const String initialRoute = '/';
   static const String signUp = '/signup';
   static const String login = '/login';
   static const String products = '/products';
   static const String categories = '/categories';
   static const String onboarding = '/onboarding';
 
-  static final routes = {
-    initial: (context) => const AuthChecker(),
-    firstPage: (context) => const Firstpage(),
-    signUp: (context) => const SingUpPage(),
-    login: (context) => LoginPage(),
-    products: (context) => const ProductPage(),
-    categories: (context) =>  HomePage(),
-    onboarding: (context) => const Onboarding(),
-  };
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case initialRoute:
+        return MaterialPageRoute(builder: (_) => const AuthChecker());
+      case onboarding:
+        return MaterialPageRoute(builder: (_) => const Onboarding());
+      case signUp:
+        return MaterialPageRoute(builder: (_) => const SingUpPage());
+      case categories:
+        return MaterialPageRoute(builder: (_) => CategoriesPage());
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(child: Text('No route defined for ${settings.name}')),
+          ),
+        );
+    }
+  }
 }
