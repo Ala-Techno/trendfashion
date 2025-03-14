12b2aba4d79ff4790ed3b14dbc0cb02640c8c4ea
Ala <alatech.dev@gmail.com>
2025-03-14 14:19:39 +0300

    refactor(app): improve routing setup and add base theme
    - Add ThemeData with scaffoldBackgroundColor set to Colors.white
    - Replace static routes with onGenerateRoute for dynamic routing
    - Rename initialRoute property to match AppRouter conventions

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,24 @@ import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:trendfashion/core/AppTheme.dart';
 import 'package:trendfashion/core/util/app_router.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:trendfashion/features/Product/data/model/ProductModel.dart';
 import 'injection_container.dart' as object;
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  //   // Initialize Hive
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  /* IF I have big data wants to be retrieve or dynamic data
+
+  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  // await Future.delayed(Duration(seconds: 5));
+  // FlutterNativeSplash.remove(); */
+
+  // Initialize Hive
   await Hive.initFlutter();
 
   // // Register your product model
   // Hive.registerAdapter(ProductModelAdapter());
-
-  // // Open a box (like a database table) for favorites
-  // await Hive.openBox<ProductModel>('favorites_box');
-
-  // final favoritesBox = Hive.box<ProductModel>('favorites_box');
-  // final favoriteManager = FavoriteManager(favoritesBox);
 
   try {
     await object.init();
@@ -37,8 +39,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'TrendFashion',
-      initialRoute: AppRouter.initial,
-      routes: AppRouter.routes,
+      theme: ThemeData(scaffoldBackgroundColor: Colors.white),
+      initialRoute: AppRouter.initialRoute,
+      onGenerateRoute: AppRouter.generateRoute,
     );
   }
 }
